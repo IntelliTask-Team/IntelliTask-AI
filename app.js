86908d06feff10bb274efe052e4393d9b832cc86
Alexis Parsat-Lacoste <alexis@parsat-lacoste.com>
2023-09-20 17:12:36 +0200

    Branch test

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require("./db");
 // https://www.npmjs.com/package/express
 const express = require("express");
 
+// Branch test comment
+
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
